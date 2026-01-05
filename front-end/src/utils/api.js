a5a0c4e5caa7eccf5b865752150d128d32549547
yobby <yobby.novialdi15@gmail.com>
2026-01-05 23:26:04 +0700

    fix

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 function getAccessToken() {
   return localStorage.getItem('accessToken');
