984a07a700b534a8ec6a6ce0a8ab24356a9c65e2
yobby <yobby.novialdi15@gmail.com>
2026-01-05 23:15:58 +0700

    add .env.example

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000'; 
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 function getAccessToken() {
   return localStorage.getItem('accessToken');
